414e0e9f557a5cbb449a2f50c328ff98248c61b2
안동연 <a56634368@gmail.com>
2023-09-05 13:15:35 +0900

    router fixed

--- builder/src/index.js
+++ builder/src/index.js
@@ -11,6 +11,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
+
     <BrowserRouter basename="/Project_CBD">
       <App />
     </BrowserRouter>
